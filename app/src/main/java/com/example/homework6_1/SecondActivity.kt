e1ebdf1e1fd57ca3adc7fbe47c1d20ae3441de74
London069 <alcoreAkaEzetight1337@gmail.com>
2023-02-23 19:06:17 +0600

    registerForResult

--- app/src/main/java/com/example/homework6_1/SecondActivity.kt
+++ app/src/main/java/com/example/homework6_1/SecondActivity.kt
@@ -7,53 +7,35 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import com.example.homework6_1.Key.Companion.DATA_KEY
 import com.example.homework6_1.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySecondBinding
-    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setData()
-        initLauncher()
         initClickers()
-    }
-
-    private fun initLauncher() {
-        resultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    binding.etText.setText(result.data?.getStringExtra(Key.DATA_KEY))
-                }
-            }
+        setData()
     }
 
     private fun setData() {
-        binding.etText.setText(intent.getStringExtra(Key.DATA_KEY))
+        binding.etText.setText(intent.getStringExtra(DATA_KEY))
     }
 
+
     private fun initClickers() {
-        with(binding) {
-            btnBack.setOnClickListener {
-                if (etText.text!!.isNotEmpty()) {
-                    passData()
-                } else {
-                    Toast.makeText(
-                        this@SecondActivity,
-                        "Edit text is empty",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
+        binding.btnBack.setOnClickListener {
+            setResult(
+                RESULT_OK,Intent().putExtra(
+                    DATA_KEY,binding.etText.text.toString()
+                )
+            )
+            finish()
         }
     }
 
-    private fun passData() {
-        val data = Intent(this@SecondActivity, MainActivity::class.java)
-        data.putExtra(Key.DATA_KEY, binding.etText.text.toString())
-        resultLauncher.launch(data)
-    }
 }
